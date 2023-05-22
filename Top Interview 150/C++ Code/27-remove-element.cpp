b40a9c767cc4c2da45900b94332db6f6b891f8b0
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-05-23 01:50:37 +0530

    Create 27-remove-element.cpp

---
+++ Top Interview 150/C++ Code/27-remove-element.cpp
@@ -0,0 +1,16 @@
+//Time:o(n)
+// Space:o(1)
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int next=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val){
+                nums[next++]=nums[i];
+            }
+        }
+        return next;
+        
+    }
+};
