90946ba98abf0e08c05cb2f61afa09bd6663a54c
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-05-23 15:59:06 +0530

    Update 55-jump-game.cpp

--- Top Interview 150/C++ Code/55-jump-game.cpp
+++ Top Interview 150/C++ Code/55-jump-game.cpp
@@ -4,6 +4,20 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n=nums.size();
+        int can_reach=0;
+        for(int i=0;i<n;i++){
+            if(can_reach<i) return false;
+            can_reach=max(can_reach,i+nums[i]);
+        }
+        return true;
+        
+    }
+};
+
+class Solution2 {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
         int current=n-1;
         for(int i=n-2;i>=0;i--){
             if(nums[i]+i>=current)
