0da88eaa206889eeea8e394ca5447eb5f4aeb706
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-06-01 16:30:27 +0530

    Create 36-valid-sudoku.java

---
+++ Top Interview 150/Java Code/matrix/36-valid-sudoku.java
@@ -0,0 +1,30 @@
+class Solution {
+    boolean check(char [][]v,int i,int j){
+        char c=v[i][j];
+        for(int x=0;x<9;x++){
+            if(x!=i && v[x][j]==c) return false;
+        }
+        for(int x=0;x<9;x++){
+            if(x!=j && v[i][x]==c) return false;
+        }
+        int ix=i/3*3;
+        int iy=j/3*3;
+        for(int x=0;x<3;x++){
+            for(int y=0;y<3;y++){
+                if(x+ix==i && y+iy==j) continue;
+                else if(v[x+ix][y+iy]==c) return false;
+            }
+        }
+        return true;
+    }
+    public boolean isValidSudoku(char[][] board) {
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]!='.') {
+                    if(check(board,i,j)==false) return false;
+                }
+            }
+        }
+        return true;
+    }
+}
