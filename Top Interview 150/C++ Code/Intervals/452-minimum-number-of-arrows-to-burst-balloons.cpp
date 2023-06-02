25d45a74788c4fdf68fb39772eb16c2b420790c2
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-06-02 17:54:30 +0530

    Update 452-minimum-number-of-arrows-to-burst-balloons.cpp

--- Top Interview 150/C++ Code/Intervals/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ Top Interview 150/C++ Code/Intervals/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -3,7 +3,6 @@ public:
     int findMinArrowShots(vector<vector<int>>& nums) {
         int cnt=1;
         sort(nums.begin(),nums.end());
-        bool flag=false;
         int back=nums[0][1];
         for(int i=1;i<nums.size();i++){
             if(back>=nums[i][0]) {
