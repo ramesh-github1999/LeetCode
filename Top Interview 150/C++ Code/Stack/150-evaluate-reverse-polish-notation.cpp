cdfea2081e0d22cb6849f99c69be4112b572265f
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-06-03 17:37:03 +0530

    Update 150-evaluate-reverse-polish-notation.cpp

--- Top Interview 150/C++ Code/Stack/150-evaluate-reverse-polish-notation.cpp
+++ Top Interview 150/C++ Code/Stack/150-evaluate-reverse-polish-notation.cpp
@@ -2,7 +2,6 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        int ans=0;
         stack<int>st;
         for(int i=0;i<tokens.size();i++){
             if(tokens[i]=="+" || tokens[i]=="-" || tokens[i]=="*" || tokens[i]=="/"){
