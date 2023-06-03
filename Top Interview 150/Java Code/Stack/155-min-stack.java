46464f36aef8296ab2e8c21bf33179eb71b0bcab
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-06-03 17:16:07 +0530

    Create 155-min-stack.java

---
+++ Top Interview 150/Java Code/Stack/155-min-stack.java
@@ -0,0 +1,35 @@
+class MinStack {
+    Stack<Integer>s1=new Stack<>();
+    Stack<Integer>s2=new Stack<>();
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        s1.push(val);
+        if(s2.empty()) s2.push(val);
+        else s2.push(Math.min(val,s2.peek()));
+    }
+    
+    public void pop() {
+        s1.pop();
+        s2.pop();
+    }
+    
+    public int top() {
+        return s1.peek();
+    }
+    
+    public int getMin() {
+        return s2.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
