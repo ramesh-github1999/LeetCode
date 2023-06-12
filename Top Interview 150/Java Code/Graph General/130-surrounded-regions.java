11fd528c05c87b7625eb262cdd929a0e42041331
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-06-12 21:24:43 +0530

    Update 130-surrounded-regions.java

--- Top Interview 150/Java Code/Graph General/130-surrounded-regions.java
+++ Top Interview 150/Java Code/Graph General/130-surrounded-regions.java
@@ -29,3 +29,37 @@ class Solution {
         }
     }
 }
+
+
+
+class Solution2 {
+    public void dfs(char[][] v,int i,int j){
+        if(i<0 || i>=v.length || j<0 || j>=v[0].length || v[i][j]=='X' || v[i][j]=='K') return;
+        v[i][j]='K';
+        dfs(v,i+1,j);
+        dfs(v,i-1,j);
+        dfs(v,i,j-1);
+        dfs(v,i,j+1);
+    }
+    
+    public void solve(char[][] v) {
+         int n=v.length;
+        int m=v[0].length;
+        // first row and last row
+        for(int j=0;j<m;j++){
+           if(v[0][j]=='O') dfs(v,0,j);
+           if(v[n-1][j]=='O') dfs(v,n-1,j);
+        }
+        // first column and last column
+        for(int i=0;i<n;i++){
+            if(v[i][0]=='O') dfs(v,i,0);
+            if(v[i][m-1]=='O')  dfs(v,i,m-1); 
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(v[i][j]=='K') v[i][j]='O';
+                else if(v[i][j]=='O') v[i][j]='X';
+            }
+        }
+    }
+}
