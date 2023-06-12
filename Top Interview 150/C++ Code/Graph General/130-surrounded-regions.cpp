77971f271b6a67c29cc2ec0b4ff33475234de2f0
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-06-12 21:19:53 +0530

    Create 130-surrounded-regions.cpp

---
+++ Top Interview 150/C++ Code/Graph General/130-surrounded-regions.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>&v,int i,int j){
+        if(i<0 || i>=v.size() || j<0 || j>=v[0].size() || v[i][j]=='X' || v[i][j]=='K') return;
+        v[i][j]='K';
+        dfs(v,i+1,j);
+        dfs(v,i-1,j);
+        dfs(v,i,j-1);
+        dfs(v,i,j+1);
+    }
+    
+    void solve(vector<vector<char>>& v) {
+        int n=v.size();
+        int m=v[0].size();
+        // first row and last row
+        for(int j=0;j<m;j++){
+            dfs(v,0,j);
+            dfs(v,n-1,j);
+        }
+        // first column and last column
+        for(int i=0;i<n;i++){
+            dfs(v,i,0);
+            dfs(v,i,m-1);
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(v[i][j]=='K') v[i][j]='O';
+                else if(v[i][j]=='O') v[i][j]='X';
+            }
+        }
+    }
+};
