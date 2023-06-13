9e2f7da7c9fb7a2d3fe703dfd72b780a8040ca8b
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-06-13 18:52:32 +0530

    Create 230-kth-smallest-element-in-a-bst.cpp

---
+++ Top Interview 150/C++ Code/Binary Search Tree/230-kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// using a vector.
+class Solution {
+public:
+   
+    void kth(TreeNode *root,vector<int>&v){
+        if(root->left!=NULL) kth(root->left,v);
+        v.push_back(root->val);
+        if(root->right!=NULL) kth(root->right,v);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int>v;
+       kth(root,v);
+        return v[k-1];
+    }
+};
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// using without any vector
+class Solution2 {
+public:
+    int ans;
+    void inorder(TreeNode *root,int &k){
+        if(!root) return;
+        inorder(root->left,k);
+        k--;
+        if(k==0){
+            ans=root->val;
+            return;
+        }
+        inorder(root->right,k);
+    }
+        
+          
+    int kthSmallest(TreeNode* root, int k) {
+         inorder(root,k);
+        return ans;
+    }
+};
