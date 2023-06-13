b83e48540e002da5896cc97c83b8d373c8feb7bf
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-06-13 19:16:04 +0530

    Update 230-kth-smallest-element-in-a-bst.cpp

--- Top Interview 150/C++ Code/Binary Search Tree/230-kth-smallest-element-in-a-bst.cpp
+++ Top Interview 150/C++ Code/Binary Search Tree/230-kth-smallest-element-in-a-bst.cpp
@@ -58,3 +58,34 @@ public:
         return ans;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+//using iteration method
+
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode *>st;
+        while(root!=NULL || !st.empty()){
+            while(root!=NULL){
+                st.push(root);
+                root=root->left;
+            }
+            root=st.top();
+            st.pop();
+            if(--k==0) return root->val;
+            root=root->right;
+        }
+        return 0;
+    }
+};
