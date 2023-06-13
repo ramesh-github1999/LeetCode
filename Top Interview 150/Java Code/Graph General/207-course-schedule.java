aa0910e47323a0aa3f3e7f8d53a3a273f6af5557
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-06-13 14:25:25 +0530

    Update 207-course-schedule.java

--- Top Interview 150/Java Code/Graph General/207-course-schedule.java
+++ Top Interview 150/Java Code/Graph General/207-course-schedule.java
@@ -1,3 +1,5 @@
+ // topological sort code 
+ // check cycle in Directed graph
 class Solution {
     public boolean canFinish(int n, int[][] v) {
         List<List<Integer>>graph=new ArrayList<>();
