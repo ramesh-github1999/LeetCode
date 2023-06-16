8e198a84cfaf995f0ae00bdc6e1b944658081420
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-06-16 23:17:12 +0530

    Create 137-single-number-ii.java

---
+++ Top Interview 150/Java Code/Bit Manipulation/137-single-number-ii.java
@@ -0,0 +1,30 @@
+// we can easily solve it by using map or sorting 
+// Bit Manipulation solution (see the leetcode solution article that is very nice).
+class Solution {
+    public int singleNumber(int[] nums) {
+        int loner=0;
+        for(int shift=0;shift<32;shift++){
+            int bitsum=0;
+            for(int i=0;i<nums.length;i++){
+                bitsum+=(nums[i]>>shift) &1; 
+            }   
+            
+            int lonerBit=bitsum%3;
+            loner=loner|(lonerBit<<shift);
+        }
+        return loner;
+    }
+}
+
+
+// Equation for Bitmask (see leetcode solution article for better understanding 
+class Solution {
+    public int singleNumber(int[] nums) {
+         int seenOnce=0,seenTwice=0;
+        for(int i=0;i<nums.length;i++){
+            seenOnce=(seenOnce^nums[i])&(~seenTwice);
+            seenTwice=(seenTwice^nums[i])&(~seenOnce);
+        }
+        return seenOnce;
+    }
+}
