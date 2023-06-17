a67db88f91d4fa0d260a6dfc6d661c664d46dbb7
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-06-17 17:30:46 +0530

    Update 918-maximum-sum-circular-subarray.cpp

--- Top Interview 150/C++ Code/Kadane's Algorithm/918-maximum-sum-circular-subarray.cpp
+++ Top Interview 150/C++ Code/Kadane's Algorithm/918-maximum-sum-circular-subarray.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
-        // find the maximum subarray sum using Kadane's algorithm
+        //Find the maximum subarray sum using Kadane's algorithm
         int n = nums.size();
         int curr_sum = nums[0], max_sum = nums[0];
         for(int i = 1; i < n; i++){
@@ -9,13 +9,13 @@ public:
             max_sum = max(curr_sum, max_sum);
         }
         
-        // find the minimum subarray sum
+        //Find the minimum subarray sum
         int curr_mini = nums[0], min_sum = nums[0];
         for(int i = 1; i < n; i++){
             curr_mini = min(curr_mini + nums[i], nums[i]);
             min_sum = min(min_sum, curr_mini);
         }
-       // our ans will be maximum of maximum subarray sum and sum-minimum subarray sum. 
+       //Our ans will be the maximum of maximum subarray sum and sum-minimum subarray sum. 
         int sum = nums[0];
         for(int i = 1; i < n; i++) sum += nums[i];
         return sum == min_sum ? max_sum : max(sum - min_sum,max_sum);
@@ -24,16 +24,16 @@ public:
 
 
 // The above code can be written in just one for loop like : 
-class Solution2 {
+class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
-        int cur_max = 0, cur_min = 0, sum = 0, max_sum = nums[0], min_sum = nums[0];
-        for (int num : nums) {
-            cur_max = max(cur_max, 0) + num;
-            max_sum = max(max_sum, cur_max);
-            cur_min = min(cur_min, 0) + num;
-            min_sum = min(min_sum, cur_min);
-            sum += num;
+        int curr_sum = nums[0], curr_min = nums[0], sum = nums[0], max_sum = nums[0], min_sum = nums[0];
+        for (int i=1;i<nums.size();i++) {
+            curr_sum=max(curr_sum+nums[i],nums[i]);
+            max_sum=max(curr_sum,max_sum);
+            curr_min=min(curr_min+nums[i],nums[i]);
+            min_sum=min(min_sum,curr_min);
+            sum += nums[i];
         }
         return sum == min_sum ? max_sum : max(max_sum, sum - min_sum);
     }
