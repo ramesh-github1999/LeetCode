22d276f78ece9948378d8f34c3eea69f3865f366
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-06-22 22:53:52 +0530

    Update 35-search-insert-position.java

--- Top Interview 150/Java Code/Binary Search/35-search-insert-position.java
+++ Top Interview 150/Java Code/Binary Search/35-search-insert-position.java
@@ -10,3 +10,19 @@ class Solution {
         return l;
     }
 }
+
+
+// Recursive solution
+class Solution {
+    int fun(int [] nums,int l,int r,int t){
+        if(l>r) return l;
+        int mid=(l+r)/2;
+        if(nums[mid]==t) return mid;
+        else if(nums[mid]>t) return fun(nums,l,mid-1,t);
+        return fun(nums,mid+1,r,t);
+    }
+    public int searchInsert(int[] nums, int target) {
+         int l=0,r=nums.length-1;
+       return fun(nums,0,r,target);
+    }
+}
