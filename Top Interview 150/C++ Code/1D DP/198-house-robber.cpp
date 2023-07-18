baec0b901b7e8c7b4c99b276266f3fccbc1d5cba
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-07-18 18:16:54 +0530

    Create 198-house-robber.cpp

---
+++ Top Interview 150/C++ Code/1D DP/198-house-robber.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+       
+        int n=nums.size();
+        if(n==1) return nums[0];
+         int maxi=max(nums[0],nums[1]);
+        int preMaxi=nums[0];
+        int dp[n];
+        dp[0]=nums[0];
+        dp[1]=nums[1];
+        for(int i=2;i<n;i++){
+            dp[i]=preMaxi+nums[i];
+            maxi=max(maxi,dp[i]);
+            preMaxi=max(preMaxi,dp[i-1]);
+        }
+        return maxi;
+    }
+};
+
+
+
+
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+    // maximum sum of non adjacent element pepcoding
+        int n=nums.size();
+        if(n==1) return nums[0];
+        int inc=nums[0],exe=0;
+        for(int i=1;i<n;i++){
+            int ninc=exe+nums[i];
+            int nexe=max(inc,exe);
+            inc=ninc;
+            exe=nexe;
+        }
+        return max(exe,inc);
+    }
+};
